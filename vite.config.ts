5d32da28c89276486bda2f0113849abadaa1849e
myfavoriteworkplace <itsmyfavoriteworkplace@gmail.com>
2026-04-24 18:36:20 +0000

    Push code without workflow file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+declare const process: { env: Record<string, string | undefined> };
+
 export default defineConfig({
-  base: "/",
+  base: process.env.BASE_PATH || "/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
